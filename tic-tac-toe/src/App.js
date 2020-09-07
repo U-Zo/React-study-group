bfb8285c99adf04f9ee1ff8e5bcdccd53f149c34
u-zo <kwoojo0127@naver.com>
2020-09-07 21:33:52 +0900

    Update tic-tac-toe game

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,19 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-const Square = () => {
+function Square(props) {
+  const [value, setValue] = useState(null);
+
   return (
     //
-    <button className="square"></button>
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
   );
-};
+}
 
-const Board = () => {
+function Board() {
+  const [state, setState] = useState({
+    squares: Array(9).fill(null),
+    xIsNext: true,
+  });
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a]; // 'O' or 'X'
+      }
+    }
+
+    return null;
+  }
+
+  function handleClick(i) {
+    const _squares = state.squares.slice();
+    if (calculateWinner(_squares) || _squares[i]) {
+      return;
+    }
+
+    _squares[i] = state.xIsNext ? 'X' : 'O';
+    setState({
+      squares: _squares,
+      xIsNext: !state.xIsNext,
+    });
+  }
+
   const renderSquare = (i) => {
-    return <Square value={i} />;
+    return <Square value={state.squares[i]} onClick={() => handleClick(i)} />;
   };
 
-  const status = 'Next player: X';
+  const winner = calculateWinner(state.squares); // X or O or null
+
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (state.xIsNext ? 'X' : 'O');
+  }
 
   return (
     <div>
@@ -35,7 +90,7 @@ const Board = () => {
       </div>
     </div>
   );
-};
+}
 
 function App() {
   return (
